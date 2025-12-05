28183ae4dddef87e804e81c5a08d9b4dd582d7cb
Lucy <solouser019@gmail.com>
2025-12-05 20:41:22 +0530

    add some css and rewrite the code

--- React-Complete-Course/04_Functions/src/Components/FormHandling.jsx
+++ React-Complete-Course/04_Functions/src/Components/FormHandling.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 
 const FormHandling = () => {
+  
   return (
-    <div>FormHandling</div>
+    <div>
+    <input type="text" placeholder="Enter your name"/>
+    <button className="p-4 bg-gray-400 font-semibold text-3xl">Change Name</button>
+    </div>
   )
 }
 
